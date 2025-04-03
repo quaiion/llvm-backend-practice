8839c4168294380167a91d2314454671d26eabd9
Korney Ivanishin <korney059@gmail.com>
2025-04-03 03:10:21 +0300

    extend QArchTargetLowering with QArchRegisterInfo for QArch toy architecture

--- llvm/lib/Target/QArch/QArchISelLowering.cpp
+++ llvm/lib/Target/QArch/QArchISelLowering.cpp
@@ -39,6 +39,27 @@ QArchTargetLowering::QArchTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   QARCH_DUMP_RED
   addRegisterClass(MVT::i32, &QArch::GPRRegClass);
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(QArch::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
 
 const char *QArchTargetLowering::getTargetNodeName(unsigned Opcode) const {
