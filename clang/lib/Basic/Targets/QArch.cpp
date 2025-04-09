bf348cfbd65513c455c910b3d9f666e9937d93a9
Korney Ivanishin <korney059@gmail.com>
2025-04-10 00:06:48 +0300

    add QArch ClangBuiltin support

--- clang/lib/Basic/Targets/QArch.cpp
+++ clang/lib/Basic/Targets/QArch.cpp
@@ -1,9 +1,16 @@
 #include "QArch.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsQArch.def"
+};
 
 void QArchTargetInfo::getTargetDefines(const LangOptions &Opts,
                                      MacroBuilder &Builder) const {
@@ -11,5 +18,6 @@ void QArchTargetInfo::getTargetDefines(const LangOptions &Opts,
 }
 
 ArrayRef<Builtin::Info> QArchTargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo,
+                        clang::QArch::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
